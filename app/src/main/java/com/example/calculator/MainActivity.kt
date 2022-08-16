fe173ad85ca5e9f2757b0bf20f93045c519ef98c
fanxin92 <851867936@qq.com>
2022-08-16 15:10:34 +0800

    landscape mode.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -65,9 +66,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting(
-                        viewModel
-                    )
+                    if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                        VerticalLayout(viewModel)
+                    } else {
+                        HorizontalLayout(viewModel)
+                    }
                 }
             }
         }
@@ -77,7 +80,7 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalUnitApi::class)
 @Suppress("StateFlowValueCalledInComposition")
 @Composable
-fun Greeting(viewModel: MainViewModel) {
+fun VerticalLayout(viewModel: MainViewModel) {
     val text = viewModel.result.collectAsState()
     Column(
         Modifier
@@ -219,6 +222,150 @@ fun Greeting(viewModel: MainViewModel) {
 }
 
 @OptIn(ExperimentalUnitApi::class)
+@Suppress("StateFlowValueCalledInComposition")
+@Composable
+fun HorizontalLayout(viewModel: MainViewModel) {
+    val text = viewModel.result.collectAsState()
+    Row(
+        Modifier
+            .fillMaxHeight()
+    ) {
+        val listState = rememberLazyListState()
+        val coroutineScope = rememberCoroutineScope()
+        LazyColumn(
+            Modifier
+                .fillMaxHeight()
+                .weight(1.0F)
+                .padding(10.dp),
+            listState,
+            verticalArrangement = Arrangement.Top
+        ) {
+            var count = 0
+            val textLayout: (TextLayoutResult) -> Unit = { result ->
+                if (result.lineCount != count) {
+                    count = result.lineCount
+                    coroutineScope.launch {
+                        viewModel.result.collectLatest {
+                            listState.animateScrollToItem(0, result.size.height)
+                        }
+                    }
+                }
+            }
+            item {
+                Text(
+                    text = text.value,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.End,
+                    fontSize = TextUnit(36F, TextUnitType.Sp),
+                    onTextLayout = textLayout
+                )
+            }
+        }
+        Column(
+            Modifier
+                .fillMaxHeight()
+                .weight(1.0F)
+                .background(if (MaterialTheme.colors.isLight) Color.White else Color(0XFF171717))
+        ) {
+            val normalModifier = Modifier
+                .weight(1.0F)
+                .fillMaxHeight()
+                .padding(10.dp)
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1F)
+            ) {
+                OperatorButton(text = "C", normalModifier) {
+                    viewModel.clear()
+                }
+                OperatorButton(text = "%", normalModifier) {
+                    viewModel.operation("%")
+                }
+                OperatorButton(text = "Del", normalModifier) {
+                    viewModel.delete()
+                }
+                LightOperatorButton(text = "÷", normalModifier) {
+                    viewModel.operation("÷")
+                }
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1F)
+            ) {
+                OperatorButton(text = "7", normalModifier) {
+                    viewModel.operation("7")
+                }
+                OperatorButton(text = "8", normalModifier) {
+                    viewModel.operation("8")
+                }
+                OperatorButton(text = "9", normalModifier) {
+                    viewModel.operation("9")
+                }
+                LightOperatorButton(text = "×", normalModifier) {
+                    viewModel.operation("×")
+                }
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1F)
+            ) {
+                OperatorButton(text = "4", normalModifier) {
+                    viewModel.operation("4")
+                }
+                OperatorButton(text = "5", normalModifier) {
+                    viewModel.operation("5")
+                }
+                OperatorButton(text = "6", normalModifier) {
+                    viewModel.operation("6")
+                }
+                LightOperatorButton(text = "-", normalModifier) {
+                    viewModel.operation("-")
+                }
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1F)
+            ) {
+                OperatorButton(text = "1", normalModifier) {
+                    viewModel.operation("1")
+                }
+                OperatorButton(text = "2", normalModifier) {
+                    viewModel.operation("2")
+                }
+                OperatorButton(text = "3", normalModifier) {
+                    viewModel.operation("3")
+                }
+                LightOperatorButton(text = "+", normalModifier) {
+                    viewModel.operation("+")
+                }
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1F)
+            ) {
+                OperatorButton(text = "00", normalModifier) {
+                    viewModel.operation("00")
+                }
+                OperatorButton(text = "0", normalModifier) {
+                    viewModel.operation("0")
+                }
+                OperatorButton(text = ".", normalModifier) {
+                    viewModel.operation(".")
+                }
+                LightOperatorButton(text = "=", normalModifier) {
+                    viewModel.calculate()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalUnitApi::class)
 @Composable
 fun OperatorButton(text: String, modifier: Modifier, onClick: () -> Unit) {
     TextButton(onClick, modifier) {
@@ -245,6 +392,6 @@ fun LightOperatorButton(text: String, modifier: Modifier, onClick: () -> Unit) {
 @Composable
 fun DefaultPreview() {
     CalculatorTheme {
-        Greeting(MainViewModel())
+        VerticalLayout(MainViewModel())
     }
 }
